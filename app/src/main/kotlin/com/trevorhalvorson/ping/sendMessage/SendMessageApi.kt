b749723a93a4044130b664d656d9b343e12eb999
Trevor Halvorson <trevor.halvorson@gmail.com>
2017-09-30 20:06:52 -0700

    Add Accept Header to fix KTOR server error
    see: https://github.com/Kotlin/ktor/issues/180

--- app/src/main/kotlin/com/trevorhalvorson/ping/sendMessage/SendMessageApi.kt
+++ app/src/main/kotlin/com/trevorhalvorson/ping/sendMessage/SendMessageApi.kt
@@ -3,10 +3,12 @@ package com.trevorhalvorson.ping.sendMessage
 import com.trevorhalvorson.ping.BuildConfig
 import io.reactivex.Observable
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface SendMessageApi {
 
+    @Headers("Accept: application/json")
     @POST(BuildConfig.MESSAGING_URL_PATH)
     fun postMessage(@Body body: SendMessageRequest): Observable<SendMessageResponse>
 
